16017f0ae93cd3dc5c05116ad40fbebec515cb6a
darylsargent <41406374+darylsargent@users.noreply.github.com>
2021-06-08 21:10:13 +1000

    Add files via upload

---
+++ data/script.js
@@ -0,0 +1,194 @@
+// Initialise websockets
+var gateway = `ws://${window.location.hostname}/ws`;
+var websocket;
+
+// Canvas objects and context objects for those canvases 
+var pcanvas=document.getElementById("pcanvas");
+var pctx=pcanvas.getContext("2d");
+var rcanvas=document.getElementById("rcanvas");
+var rctx=rcanvas.getContext("2d");
+
+// Image objects
+var imageSide=document.createElement("img");
+var imageBack=document.createElement("img");
+
+// Calibration and calculation objects
+var rollDeviation = 0;
+var pitchDeviation = 0; 
+var wheelbase = 2000;
+var calibration;
+var rollValue = "0";
+var pitchValue = "0";
+var heightAdjust = "0";
+
+// On page load resize canvas objects to fit cards in grid
+imageSide.onload=function(){
+    cardDim = document.getElementById("pitchCard").clientWidth;
+    pcanvas.width = cardDim-3;
+    pcanvas.height = cardDim-3;
+    pctx.drawImage(imageSide,0,0,pcanvas.width,pcanvas.height);
+    cardDim = document.getElementById("rollCard").clientWidth;
+    rcanvas.width = cardDim-3;
+    rcanvas.height = cardDim-3;
+    rctx.drawImage(imageBack,0,0,rcanvas.width,rcanvas.height);
+}
+
+// Load images for roll and pich view -
+// square images 512px x 512px with transparent background work best
+imageSide.src="van_side.png";
+imageSide.class="image1";
+imageBack.src="van_back.png";
+imageBack.class="image1";
+
+// Add listener for page load 
+window.addEventListener('load', onload);
+
+// Intialise websocket on page load
+function onload(event) {
+    initWebSocket();
+}
+
+// Function requests update by sending message
+// "getValues"
+function getValues(){
+    websocket.send("getValues");
+}
+
+// Set up websocket events
+function initWebSocket() {
+    console.log('Trying to open a WebSocket connection…');
+    websocket = new WebSocket(gateway);
+    websocket.onopen = onOpen;
+    websocket.onclose = onClose;
+    websocket.onmessage = onMessage;
+}
+
+// This actually does most of the work, when websocket is established
+// this function wil request an update every 300 msecs, will only stop
+// when page is refreshed or closed.
+function onOpen(event) {
+    console.log('Connection opened');
+    let timerID = setInterval(getValues, 300 );   
+} 
+
+// Websocket close event
+function onClose(event) {
+    console.log('Connection closed');
+    setTimeout(initWebSocket, 2000);
+}
+
+// Validate form data before sending to server
+function wsSubmitForm() {
+    // Display warning message - because we will zero the angles to their current settings
+    if (confirm("Warning: Before clicking OK, ensure device is firmly mounted to a" +
+                " known level surface.  Displayed angles will be zeroed to their current values.")) {
+        // build JSON string of deviation and wheelbase.  If the user didn't change them they still
+        // will be sent but obviously ony the zero adjust will take place.
+        calibration = '{"rollDeviation":"' + document.forms["calibration"]["rollDeviation"].value +
+            '","pitchDeviation":"' + document.forms["calibration"]["pitchDeviation"].value +
+            '","wheelbase":"' +  document.forms["calibration"]["wheelbase"].value + '"}';
+        // send to server
+        websocket.send(calibration);
+    } else {
+        // We clicked Cancel
+        alert("No calibration done");
+        return;
+    }
+}
+
+// Generic message event as we only ever expect one type of
+// message i.e. a JSON string in the format:
+// {"rollValue":"nn.n","pitchValue":"nn.n","rollDeviation":"nn.n","pitchDeviation":"nn.n","wheelbase":"nnn"}
+function onMessage(event) {
+    console.log(event.data);
+    var myObj = JSON.parse(event.data);
+    // Extract angles from JSON and write angles
+    // note we write 0.0 for angles between -0.1 and 0.1 because they display with 
+    // one decimal place - avoids value -0.0 displaying
+    if ((parseFloat(myObj.rollValue) > -0.1) && (parseFloat(myObj.rollValue) < 0.1)) {
+        rollValue = "Roll Angle: 0.0º";
+    } else {
+        rollValue = "Roll Angle: " + myObj.rollValue + "º";
+    }
+    if ((parseFloat(myObj.pitchValue) > -0.1) && (parseFloat(myObj.pitchValue) < 0.1)) {
+        pitchValue = "Pitch Angle: 0.0º";
+    } else {
+        pitchValue = "Pitch Angle: " + myObj.pitchValue + "º";
+    }
+    // Draw rotated image - roll is reversed as we are looking from back
+    drawPitchRotated(parseFloat(myObj.pitchValue));
+    drawRollRotated(-parseFloat(myObj.rollValue));
+    // Populate form values if they have changed
+    // extract previous calibration data from JSON
+    if (wheelbase !== myObj.wheelbase) {
+        wheelbase = myObj.wheelbase;
+        document.getElementById("wbase").value = wheelbase;
+    }
+    if (rollDeviation !== myObj.rollDeviation) {
+        rollDeviation = myObj.rollDeviation;
+        document.getElementById("rollD").value = rollDeviation;
+    }
+    if (pitchDeviation !== myObj.pitchDeviation) {
+        pitchDeviation = myObj.pitchDeviation;
+        document.getElementById("pitchD").value = pitchDeviation;
+    }
+    // Height adjustment calc
+    x = wheelbase * 2 * Math.sin(myObj.rollValue/2*Math.PI/180);
+    x = x * Math.sin((180-myObj.rollValue)/2*Math.PI/180);
+    if (x < 0) {
+        heightAdjust =  "Raise right " +  Math.round(Math.abs(x)) + " cm";
+    } else {
+        heightAdjust = "Raise left " +  Math.round(Math.abs(x)) + " cm";
+    }
+}
+
+// Draw image on canvas for pitch (side view)
+function drawPitchRotated(degrees){
+    // Clear canvas, set canvas backgroud if we are "level".  
+    // Deviation values are saved in SPIFFS and passed from ESP to client
+    pctx.textAlign="center";
+    pctx.clearRect(0,0,pcanvas.width,pcanvas.height);
+    if ((degrees >= -pitchDeviation) && (degrees <= pitchDeviation)) {
+      pctx.fillStyle="green";
+    } else {
+      pctx.fillStyle="ivory";
+    }
+    // Values written to canvas before we rotate
+    pctx.fillRect(0,0,pcanvas.width,pcanvas.height);
+    pctx.font = pcanvas.width/16 + "px Arial";
+    pctx.fillStyle="darkblue";
+    pctx.fillText(pitchValue,pcanvas.width/2,pcanvas.height*0.1);
+    pctx.save();
+    // Set origin of canvas to centre of image because rotate 
+    // method rotates around origin, normally top left
+    pctx.translate(pcanvas.width/2,pcanvas.height/2);
+    // Rotate canvas by angle in radians
+    pctx.rotate(degrees*Math.PI/180);
+    // Draw image on canvas, starting at top left of canvas
+    // If we used 0,0 as draw coordinates it would draw from
+    // centre (the new origin we set above)
+    pctx.drawImage(imageSide,-pcanvas.width/2,-pcanvas.height/2,pcanvas.width,pcanvas.height);
+    pctx.restore();
+}
+
+// Draw image on convas for roll (back view)
+// All other comments as per pitch function above
+function drawRollRotated(degrees){
+    rctx.textAlign="center";
+    rctx.clearRect(0,0,rcanvas.width,rcanvas.height);
+    if ((degrees >= -rollDeviation) && (degrees <= rollDeviation)) {
+      rctx.fillStyle="green";
+    } else {
+      rctx.fillStyle="ivory";
+    }
+    rctx.fillRect(0,0,rcanvas.width,rcanvas.height);
+    rctx.font = rcanvas.width/16 + "px Arial";
+    rctx.fillStyle="darkblue";
+    rctx.fillText(rollValue,rcanvas.width/2,rcanvas.height*0.1);
+    rctx.fillText(heightAdjust,rcanvas.width/2,rcanvas.height*0.95);
+    rctx.save();
+    rctx.translate(rcanvas.width/2,rcanvas.height/2);
+    rctx.rotate(degrees*Math.PI/180);
+    rctx.drawImage(imageBack,-rcanvas.width/2,-rcanvas.height/2,rcanvas.width,rcanvas.height);
+    rctx.restore();
+}
---
+++ mpu_6050_roll_pitch.ino
@@ -0,0 +1,317 @@
+// Absolute angle readings from Adafruit MPU6050
+// Served to web page via websocket connection
+
+// Libraries for MPU6050
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+
+// Libraries for web page / websockets
+#include <ESPmDNS.h>
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <SPIFFS.h>
+#include <Arduino_JSON.h>
+
+// Wifi credentials (note we will use AP mode when finished)
+const char* ssid = "LEVEL";
+const char* password = "apasswordforanAP";
+
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+AsyncWebSocket ws("/ws");
+
+// Initialise MPU
+Adafruit_MPU6050 mpu;
+
+// JSON variables
+float roll, pitch;
+JSONVar angleValues;
+JSONVar calibration;
+JSONVar calibrationUpdate;
+
+// Variables used for display and calculation
+String message = "";
+String rollValue = "0";
+String pitchValue = "0";
+String rollAdjust, pitchAdjust, rollDeviation, pitchDeviation, wheelbase, temp;
+
+// Read claibration values from files
+void readCalibration() {
+  File calibrationFile = SPIFFS.open("/calibration.json", "r");
+  if(!calibrationFile){ 
+    Serial.println("Failed to open config file for reading"); 
+    return; 
+  }
+  // sanity check to make sure file is sort of valid
+  size_t size = calibrationFile.size();
+  if (size > 1024) {
+    Serial.println("Calibration file too large");
+    return;
+  }
+  // Load JSON array from file, JSON string must be single line
+  //e.g. {"rollDeviation":0.5,"pitchDeviation":1.2,"rollAdjust":0,"pitchAdjust":0,"wheelbase":237}
+  calibration = JSON.parse(calibrationFile.readString());
+  // load into some strings s we can use them in other functions
+  rollDeviation = calibration["rollDeviation"];
+  pitchDeviation = calibration["pitchDeviation"];
+  rollAdjust = calibration["rollAdjust"];
+  pitchAdjust = calibration["pitchAdjust"];
+  wheelbase = calibration["wheelbase"];
+  // close file
+  calibrationFile.close();
+ }
+
+void writeCalibration(){
+  File calibrationFile = SPIFFS.open("/calibration.json", "w");
+  if(!calibrationFile){ 
+    Serial.println("Failed to open config file for write"); 
+    return; 
+  }
+  // write data
+  calibrationFile.print(JSON.stringify(calibration));
+  // close file
+  calibrationFile.close();
+ }
+
+// Convert roll and pitch angle to JSON string
+String setAngleValues(){
+  angleValues["rollValue"] = String(roll,1);
+  angleValues["pitchValue"] = String(pitch,1);
+  // we pass the values used for calculations each time
+  angleValues["rollDeviation"] = rollDeviation;
+  angleValues["pitchDeviation"] = pitchDeviation;
+  angleValues["wheelbase"] = wheelbase;  
+  String jsonString = JSON.stringify(angleValues);
+  return jsonString;
+}
+
+void initMDNS() {
+  if(!MDNS.begin("LevelGuage")) {
+     Serial.println("Error starting mDNS");
+     return;
+  }
+  MDNS.addService("http", "tcp", 80);
+}
+
+// Initialise SPIFFS - use No-OTA 2MB APP 2MB SPIFFS partition scheme
+void initFS() {
+  if (!SPIFFS.begin()) {
+    Serial.println("An error has occurred while mounting SPIFFS");
+  }
+  else{
+   Serial.println("SPIFFS mounted successfully");
+  }
+}
+
+// Start WIFI - note STAtion mode will be replaced with AP mode
+void initWiFi() {
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP(ssid, password);
+  delay(1000);
+  Serial.println(WiFi.softAPIP());
+}
+
+// Websocket send message to client
+void notifyClients(String angleValues) {
+  ws.textAll(angleValues);
+}
+
+// Websocket receive message from client
+void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
+  AwsFrameInfo *info = (AwsFrameInfo*)arg;
+  if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
+    data[len] = 0;
+    message = (char*)data;
+    // Handle request for roll and pitch data
+    if (strcmp((char*)data, "getValues") == 0) {
+      // Read data from MPU
+      sensors_event_t a, g, temp;
+      mpu.getEvent(&a, &g, &temp);
+      // Calculate absolute angles 
+        roll = (atan2(a.acceleration.y, a.acceleration.z) * 180.0 / PI)-rollAdjust.toFloat();
+        pitch = (atan2(-a.acceleration.x, sqrt(a.acceleration.y * a.acceleration.y + a.acceleration.z *a.acceleration.z)) *180.0 / PI)-pitchAdjust.toFloat();
+      // Call function to send websockett data to client
+      notifyClients(setAngleValues());
+    } else {
+      // Anything other than "getValues" is JSON calibration data
+      // load JSON array - these values passed from web page
+      calibrationUpdate = JSON.parse(message);
+      // set deviation and wheelbase to data in message from client
+      temp = calibrationUpdate["rollDeviation"];
+      // Note the Arduino_JSON library will not let you assign one value directly
+      // to another - intermediate step is required
+      calibration["rollDeviation"] = temp;
+      temp = calibrationUpdate["pitchDeviation"];
+      calibration["pitchDeviation"] = temp;
+      temp = calibrationUpdate["wheelbase"];
+      calibration["wheelbase"] = temp; 
+      // Set adjustment figures to current roll and pitch value i.e. 
+      // those values will now return zero.
+      sensors_event_t a, g, temp;
+      mpu.getEvent(&a, &g, &temp);
+      calibration["rollAdjust"] = String(atan2(a.acceleration.y, a.acceleration.z) * 180.0 / PI);
+      calibration["pitchAdjust"] = String(atan2(-a.acceleration.x, sqrt(a.acceleration.y * a.acceleration.y + a.acceleration.z *a.acceleration.z)) *180.0 / PI);
+      // Save calibration data to file
+      writeCalibration();
+      // Update values to new ones
+      rollDeviation = calibration["rollDeviation"];
+      pitchDeviation = calibration["pitchDeviation"];
+      rollAdjust = calibration["rollAdjust"];
+      pitchAdjust = calibration["pitchAdjust"];
+      wheelbase = calibration["wheelbase"];
+    }
+  }
+}
+
+// Websocket global event handler -  note no error handling!
+void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len) {
+  switch (type) {
+    case WS_EVT_CONNECT:
+      Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
+      Serial.println(roll, pitch);
+      break;
+    case WS_EVT_DISCONNECT:
+      Serial.printf("WebSocket client #%u disconnected\n", client->id());
+      break;
+    case WS_EVT_DATA:
+      handleWebSocketMessage(arg, data, len);
+      break;
+    case WS_EVT_PONG:
+    case WS_EVT_ERROR:
+      break;
+  }
+}
+
+// Initialise websocket - points to event handler function
+void initWebSocket() {
+  ws.onEvent(onEvent);
+  server.addHandler(&ws);
+}
+
+void setup(void) {
+  Serial.begin(115200);
+  while (!Serial)
+    delay(10); // pause for serial console - only needed for debugging
+  // Start up SPIFFS, WiFi then Websockets
+  initFS();
+  readCalibration();
+  initWiFi();
+  initWebSocket();
+  initMDNS();
+
+  // Web Server Root URL
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/index.html", "text/html");
+  });
+  server.serveStatic("/", SPIFFS, "/");
+
+  // Try to find MPU
+  if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) {
+      delay(10);
+    }
+  }
+  Serial.println("MPU6050 Found!");
+
+  // On startup output some info re the MPU to the console
+  // Fine tune sensitivity by changing the range
+  // Smaller is more sensitive
+  mpu.setAccelerometerRange(MPU6050_RANGE_16_G);
+  Serial.print("Accelerometer range set to: ");
+  switch (mpu.getAccelerometerRange()) {
+  case MPU6050_RANGE_2_G:
+    Serial.println("+-2G");
+    break;
+  case MPU6050_RANGE_4_G:
+    Serial.println("+-4G");
+    break;
+  case MPU6050_RANGE_8_G:
+    Serial.println("+-8G");
+    break;
+  case MPU6050_RANGE_16_G:
+    Serial.println("+-16G");
+    break;
+  }
+  // Note Gyro not used in this app, so deg/s setting not relevant
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+  Serial.print("Gyro range set to: ");
+  switch (mpu.getGyroRange()) {
+  case MPU6050_RANGE_250_DEG:
+    Serial.println("+- 250 deg/s");
+    break;
+  case MPU6050_RANGE_500_DEG:
+    Serial.println("+- 500 deg/s");
+    break;
+  case MPU6050_RANGE_1000_DEG:
+    Serial.println("+- 1000 deg/s");
+    break;
+  case MPU6050_RANGE_2000_DEG:
+    Serial.println("+- 2000 deg/s");
+    break;
+  }
+  /* Noise filter bandwidth.  The smaller the setting the less
+     response to fast changes which could be noise, but data 
+     update takes slightly longer.  That may be important in a 
+     quadcopter but shouldn't matter here. */
+  mpu.setFilterBandwidth(MPU6050_BAND_5_HZ);
+  Serial.print("Filter bandwidth set to: ");
+  switch (mpu.getFilterBandwidth()) {
+  case MPU6050_BAND_260_HZ:
+    Serial.println("260 Hz");
+    break;
+  case MPU6050_BAND_184_HZ:
+    Serial.println("184 Hz");
+    break;
+  case MPU6050_BAND_94_HZ:
+    Serial.println("94 Hz");
+    break;
+  case MPU6050_BAND_44_HZ:
+    Serial.println("44 Hz");
+    break;
+  case MPU6050_BAND_21_HZ:
+    Serial.println("21 Hz");
+    break;
+  case MPU6050_BAND_10_HZ:
+    Serial.println("10 Hz");
+    break;
+  case MPU6050_BAND_5_HZ:
+    Serial.println("5 Hz");
+    break;
+  }
+
+  Serial.println("");
+  delay(100);
+
+  // On startup we get the values of the sensors just so we can
+  // print them to the console
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+  Serial.print("Temperature: ");
+  Serial.print(temp.temperature);
+  Serial.println(" degC");
+
+  Serial.println(""); 
+
+  // Roll and pitch calculated from acceleration values
+  roll = (atan2(a.acceleration.y, a.acceleration.z) * 180.0 / PI)-rollAdjust.toFloat();
+  pitch = (atan2(-a.acceleration.x, sqrt(a.acceleration.y * a.acceleration.y + a.acceleration.z *a.acceleration.z)) *180.0 / PI)-pitchAdjust.toFloat();
+  Serial.print("Roll: ");
+  Serial.print(roll,1);
+  Serial.print("   Pitch: ");
+  Serial.print(pitch,1);
+  Serial.print("\r");
+
+  // Start web server
+  server.begin();
+}
+
+// Main code doesn't do much - all processing done by 
+// Javascript from client and websocket requests triggering functions
+void loop() {
+
+  ws.cleanupClients();
+
+}
